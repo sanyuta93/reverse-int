560c1ea3ba78ea66a9e531a24a12062a4bcd1eb3
alexandra.harko <sanyuta93@gmail.com>
2021-02-08 13:55:35 +0300

    init: task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports =   function reverse (n) {
+    if (n < 0) {
+      n = n*(-1);
+    }
+    let numText = String(n);
+    let arr_n = numText.split('');
+    let arr_n1 = arr_n.reverse();
+    let arr_n2 = arr_n1.join('');
+    let result = Number(arr_n2);
+    return result;
 }
